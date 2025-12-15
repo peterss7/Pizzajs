63a80488149268c95e8ce4b5a9178ad86b10b0a4
peterss7 <peterss7405926@gmail.com>
2025-12-15 15:25:56 -0500

    git extension is annoying

--- frontend/src/features/app/pages/App.tsx
+++ frontend/src/features/app/pages/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
-import { AccountProvider } from './stores/useAccountContext'
-import PizzaCanvas from './pages/PizzaCanvas'
-import { useAuth } from './auth/useAuth'
-import { AuthProvider } from './auth/AuthProvider';
-import LoginPage from './features/auth/pages/LoginPage';
+import { AccountProvider } from '../../../stores/useAccountContext'
+import PizzaCanvas from '../../../pages/PizzaCanvas'
+import { useAuth } from '../../../auth/useAuth'
+import { AuthProvider } from '../../../auth/AuthProvider';
+import LoginPage from '../../auth/pages/LoginPage';
 
 export default function App() {
   const { isLoading, isAuthenticated } = useAuth();
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+import App from './features/app/pages/App'
 import { AuthProvider } from './auth/AuthProvider'
 import { BrowserRouter } from 'react-router-dom'
 
